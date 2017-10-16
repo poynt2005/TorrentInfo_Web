a1f30d5729d0c831c39af166d4182f98e523a1ab
Steve_Chung <poynt2010@hotmail.com>
2017-10-17 07:23:43 +0800

    Change form submit to ajax , add spinner effect

--- static/js/control.js
+++ static/js/control.js
@@ -1,4 +1,33 @@
 $(document).ready(function(){
+	
+	//spinner 
+	var opts = {
+		  lines: 13 // The number of lines to draw
+		, length: 28 // The length of each line
+		, width: 14 // The line thickness
+		, radius: 42 // The radius of the inner circle
+		, scale: 1 // Scales overall size of the spinner
+		, corners: 1 // Corner roundness (0..1)
+		, color: '#000' // #rgb or #rrggbb or array of colors
+		, opacity: 0.25 // Opacity of the lines
+		, rotate: 0 // The rotation offset
+		, direction: 1 // 1: clockwise, -1: counterclockwise
+		, speed: 1 // Rounds per second
+		, trail: 60 // Afterglow percentage
+		, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+		, zIndex: 2e9 // The z-index (defaults to 2000000000)
+		, className: 'spinner' // The CSS class to assign to the spinner
+		, top: '50%' // Top position relative to parent
+		, left: '50%' // Left position relative to parent
+		, shadow: false // Whether to render a shadow
+		, hwaccel: false // Whether to use hardware acceleration
+		, position: 'absolute' // Element positioning
+	};
+	//spinner target
+	var spinner = new Spinner(opts);
+	var target = $("body").get(0);
+	
+	
 	$("#post_f").change(function(){
 		let FileName = $(this).val()
 		
@@ -17,7 +46,11 @@ $(document).ready(function(){
 				url : "/file_process",
 				processData: false,
 				contentType: false,
+				beforeSend : function () {
+						spinner.spin(target);                    
+				},
 				success: function(data) {
+					spinner.spin();
 					$("#magnet_result").html(data.link);
 				},
 			});
@@ -32,9 +65,32 @@ $(document).ready(function(){
 		if(!result)
 			alert("It's not a valid magnet link!");
 		else{
-			$("#magnet_form").submit();
-		}
-	})
+			
+				let form_data = $("#magnet_form");
+				
+				$.ajax({
+					type : "POST",
+					data : form_data.serialize(),
+					url : "/magnet_process",
+					
+					beforeSend : function () {
+						spinner.spin(target);                    
+					},
+					
+					success: function(data){
+						let file = new Blob([data] , {type : "application/octet-stream"});
+						downloadURL = window.URL.createObjectURL(file);
+						html_element = "<a href ='" + downloadURL + "' download = 'YourTorrent.torrent'>Your File is Ready</a>";
+						spinner.spin();
+						$("#torrent_result").html(html_element);
+					},
+			
+				});
+				
+				/*("#magnet_form").submit();*/
+			
+		};
+	});
 	
 	
 	$("#file_box").click(function (){
