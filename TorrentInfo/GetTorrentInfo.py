aa4f9d7beb288637b25d19a3fa93b7c7a8d18db5
Steve_Chung <poynt2010@hotmail.com>
2017-10-28 21:09:30 +0800

    Update GetTorrentInfo.py

--- TorrentInfo/GetTorrentInfo.py
+++ TorrentInfo/GetTorrentInfo.py
@@ -1,42 +1,15 @@
 #coding = utf-8
 import libtorrent
-import sys
-import tempfile
-import os
-import time
-import re
+from urllib import urlencode
+
 class GetTorrentInfo(object):
     def __init__(self , readbinary):
         self.bt = libtorrent.bdecode(readbinary)
     
     def get_magnet(self):
         info = libtorrent.torrent_info(self.bt)
-        name = ''
-        if ' ' in info.name():
-            name = info.name().replace(' ', '_')
-        else:
-            name = info.name()
 
-        url_link = 'magnet:?xt=urn:btih:%s' % (info.info_hash())
+        name_url_code = urlencode({'dn' : info.name()})
+
+        url_link = 'magnet:?xt=urn:btih:%s&%s' % (info.info_hash(), name_url_code)
         return url_link
-
-    #check if the torrent has directories
-    @staticmethod
-    def hasDirectory(input_data):
-        if re.match('.*path.*' , str(input_data)):
-            return True
-        else:
-            return False
-
-    def getFileName(self):
-        #it's a dictionary data , get files' name
-        info_dict = self.bt['info']
-        if self.hasDirectory(info_dict):
-            files_list = info_dict['files']
-            result = []
-            for i in files_list:
-                result.append(i['path'])
-            return result
-        else:
-            return info_dict['name']
-        
